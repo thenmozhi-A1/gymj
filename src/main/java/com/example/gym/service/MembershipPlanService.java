e60af65859b5ba68b7a47b5b2f734df5b858433b
thenmozhi-A1 <thenmozhiayothi@gmail.com>
2026-06-15 12:35:52 +0530

    Update code

--- src/main/java/com/example/gym/service/MembershipPlanService.java
+++ src/main/java/com/example/gym/service/MembershipPlanService.java
@@ -4,11 +4,13 @@ import com.example.gym.entity.MembershipPlan;
 import com.example.gym.repository.MembershipPlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MembershipPlanService {
 
     private final MembershipPlanRepository membershipPlanRepository;
